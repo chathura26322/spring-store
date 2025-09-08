d30bb28e11a7bc6862941cdf5aff7de162601c9b
Chathura Dilshantha Kumara <chathura26322@gmail.com>
2025-09-08 09:18:55 +0530

    add the migrations

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -11,8 +11,6 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context=SpringApplication.run(StoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "Mosh", "12345", "mosh@codewithmosh.com"));
-        userService.registerUser(new User(1L, "Chathura", "12345", "Chathura@gmail.com"));
+
     }
 }
